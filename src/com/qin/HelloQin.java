01647277b2c5506df900c08268696f7fcc62b6a3
Administrator <qinzengxin@163.com>
2020-02-17 23:15:34 +0800

    第二次测试

--- src/com/qin/HelloQin.java
+++ src/com/qin/HelloQin.java
@@ -6,13 +6,13 @@ public class HelloQin {
 
     public static void main(String[] args){
         System.out.println("Hello qin秦增鑫!!");
-
+        System.out.println("Hello qin秦增鑫!!");
         ArrayList list = new ArrayList();
     }
 
    /* public void qin(){
 
-        //这是注释
+        //这是注释哈哈哈
 
     }*/
 }
---
+++ webs/src/nihao.java
@@ -0,0 +1,6 @@
+/**
+ * @author Qinzengxin
+ * @create on 2020-02-17 下午 11:12
+ */
+public class nihao {
+}
